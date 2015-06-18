9028c133e581e5da8d0aba551f4408a4ce4b77bf
lccaseiro58 <lccaseiro58@gmail.com>
2015-06-18 14:04:57 +0100

    friendly

---
+++ FN/mrmpi-7Apr14/src/Obj_mpicc/cmapreduce.d
@@ -0,0 +1,117 @@
+cmapreduce.o: cmapreduce.cpp /usr/include/stdc-predef.h cmapreduce.h \
+ /usr/include/mpi.h \
+ /usr/lib/gcc/x86_64-unknown-linux-gnu/5.1.0/include/stddef.h \
+ /usr/include/mpi_portable_platform.h \
+ /usr/include/openmpi/ompi/mpi/cxx/mpicxx.h /usr/include/c++/5.1.0/map \
+ /usr/include/c++/5.1.0/bits/stl_tree.h \
+ /usr/include/c++/5.1.0/bits/stl_algobase.h \
+ /usr/include/c++/5.1.0/x86_64-unknown-linux-gnu/bits/c++config.h \
+ /usr/include/c++/5.1.0/x86_64-unknown-linux-gnu/bits/os_defines.h \
+ /usr/include/features.h /usr/include/sys/cdefs.h \
+ /usr/include/bits/wordsize.h /usr/include/gnu/stubs.h \
+ /usr/include/gnu/stubs-64.h \
+ /usr/include/c++/5.1.0/x86_64-unknown-linux-gnu/bits/cpu_defines.h \
+ /usr/include/c++/5.1.0/bits/functexcept.h \
+ /usr/include/c++/5.1.0/bits/exception_defines.h \
+ /usr/include/c++/5.1.0/bits/cpp_type_traits.h \
+ /usr/include/c++/5.1.0/ext/type_traits.h \
+ /usr/include/c++/5.1.0/ext/numeric_traits.h \
+ /usr/include/c++/5.1.0/bits/stl_pair.h \
+ /usr/include/c++/5.1.0/bits/move.h \
+ /usr/include/c++/5.1.0/bits/concept_check.h \
+ /usr/include/c++/5.1.0/bits/stl_iterator_base_types.h \
+ /usr/include/c++/5.1.0/bits/stl_iterator_base_funcs.h \
+ /usr/include/c++/5.1.0/debug/debug.h \
+ /usr/include/c++/5.1.0/bits/stl_iterator.h \
+ /usr/include/c++/5.1.0/bits/ptr_traits.h \
+ /usr/include/c++/5.1.0/bits/predefined_ops.h \
+ /usr/include/c++/5.1.0/bits/allocator.h \
+ /usr/include/c++/5.1.0/x86_64-unknown-linux-gnu/bits/c++allocator.h \
+ /usr/include/c++/5.1.0/ext/new_allocator.h /usr/include/c++/5.1.0/new \
+ /usr/include/c++/5.1.0/exception \
+ /usr/include/c++/5.1.0/bits/atomic_lockfree_defines.h \
+ /usr/include/c++/5.1.0/bits/memoryfwd.h \
+ /usr/include/c++/5.1.0/bits/stl_function.h \
+ /usr/include/c++/5.1.0/backward/binders.h \
+ /usr/include/c++/5.1.0/ext/alloc_traits.h \
+ /usr/include/c++/5.1.0/bits/stl_map.h \
+ /usr/include/c++/5.1.0/bits/stl_multimap.h \
+ /usr/include/c++/5.1.0/bits/range_access.h \
+ /usr/include/c++/5.1.0/utility /usr/include/c++/5.1.0/bits/stl_relops.h \
+ /usr/lib/gcc/x86_64-unknown-linux-gnu/5.1.0/include/stdarg.h \
+ /usr/include/stdio.h /usr/include/bits/types.h \
+ /usr/include/bits/typesizes.h /usr/include/libio.h \
+ /usr/include/_G_config.h /usr/include/wchar.h \
+ /usr/include/bits/stdio_lim.h /usr/include/bits/sys_errlist.h \
+ /usr/include/bits/stdio.h /usr/include/c++/5.1.0/iostream \
+ /usr/include/c++/5.1.0/ostream /usr/include/c++/5.1.0/ios \
+ /usr/include/c++/5.1.0/iosfwd /usr/include/c++/5.1.0/bits/stringfwd.h \
+ /usr/include/c++/5.1.0/bits/postypes.h /usr/include/c++/5.1.0/cwchar \
+ /usr/include/bits/wchar.h /usr/include/xlocale.h \
+ /usr/include/c++/5.1.0/bits/char_traits.h \
+ /usr/include/c++/5.1.0/bits/localefwd.h \
+ /usr/include/c++/5.1.0/x86_64-unknown-linux-gnu/bits/c++locale.h \
+ /usr/include/c++/5.1.0/clocale /usr/include/locale.h \
+ /usr/include/bits/locale.h /usr/include/c++/5.1.0/cctype \
+ /usr/include/ctype.h /usr/include/endian.h /usr/include/bits/endian.h \
+ /usr/include/bits/byteswap.h /usr/include/bits/byteswap-16.h \
+ /usr/include/c++/5.1.0/bits/ios_base.h \
+ /usr/include/c++/5.1.0/ext/atomicity.h \
+ /usr/include/c++/5.1.0/x86_64-unknown-linux-gnu/bits/gthr.h \
+ /usr/include/c++/5.1.0/x86_64-unknown-linux-gnu/bits/gthr-default.h \
+ /usr/include/pthread.h /usr/include/sched.h /usr/include/time.h \
+ /usr/include/bits/sched.h /usr/include/bits/time.h \
+ /usr/include/bits/timex.h /usr/include/bits/pthreadtypes.h \
+ /usr/include/bits/setjmp.h \
+ /usr/include/c++/5.1.0/x86_64-unknown-linux-gnu/bits/atomic_word.h \
+ /usr/include/c++/5.1.0/bits/locale_classes.h \
+ /usr/include/c++/5.1.0/string \
+ /usr/include/c++/5.1.0/bits/ostream_insert.h \
+ /usr/include/c++/5.1.0/bits/cxxabi_forced.h \
+ /usr/include/c++/5.1.0/bits/basic_string.h \
+ /usr/include/c++/5.1.0/bits/basic_string.tcc \
+ /usr/include/c++/5.1.0/bits/locale_classes.tcc \
+ /usr/include/c++/5.1.0/stdexcept /usr/include/c++/5.1.0/streambuf \
+ /usr/include/c++/5.1.0/bits/streambuf.tcc \
+ /usr/include/c++/5.1.0/bits/basic_ios.h \
+ /usr/include/c++/5.1.0/bits/locale_facets.h \
+ /usr/include/c++/5.1.0/cwctype /usr/include/wctype.h \
+ /usr/include/c++/5.1.0/x86_64-unknown-linux-gnu/bits/ctype_base.h \
+ /usr/include/c++/5.1.0/bits/streambuf_iterator.h \
+ /usr/include/c++/5.1.0/x86_64-unknown-linux-gnu/bits/ctype_inline.h \
+ /usr/include/c++/5.1.0/bits/locale_facets.tcc \
+ /usr/include/c++/5.1.0/bits/basic_ios.tcc \
+ /usr/include/c++/5.1.0/bits/ostream.tcc /usr/include/c++/5.1.0/istream \
+ /usr/include/c++/5.1.0/bits/istream.tcc \
+ /usr/include/openmpi/ompi/mpi/cxx/constants.h \
+ /usr/include/openmpi/ompi/mpi/cxx/functions.h \
+ /usr/include/openmpi/ompi/mpi/cxx/datatype.h \
+ /usr/include/openmpi/ompi/mpi/cxx/exception.h \
+ /usr/include/openmpi/ompi/mpi/cxx/op.h \
+ /usr/include/openmpi/ompi/mpi/cxx/status.h \
+ /usr/include/openmpi/ompi/mpi/cxx/request.h \
+ /usr/include/openmpi/ompi/mpi/cxx/group.h \
+ /usr/include/openmpi/ompi/mpi/cxx/comm.h \
+ /usr/include/openmpi/ompi/mpi/cxx/win.h \
+ /usr/include/openmpi/ompi/mpi/cxx/file.h \
+ /usr/include/openmpi/ompi/mpi/cxx/errhandler.h \
+ /usr/include/openmpi/ompi/mpi/cxx/intracomm.h \
+ /usr/include/openmpi/ompi/mpi/cxx/topology.h \
+ /usr/include/openmpi/ompi/mpi/cxx/intercomm.h \
+ /usr/include/openmpi/ompi/mpi/cxx/info.h \
+ /usr/include/openmpi/ompi/mpi/cxx/datatype_inln.h \
+ /usr/include/openmpi/ompi/mpi/cxx/functions_inln.h /usr/include/string.h \
+ /usr/include/openmpi/ompi/mpi/cxx/request_inln.h \
+ /usr/include/openmpi/ompi/mpi/cxx/comm_inln.h \
+ /usr/include/openmpi/ompi/mpi/cxx/intracomm_inln.h \
+ /usr/include/openmpi/ompi/mpi/cxx/topology_inln.h \
+ /usr/include/openmpi/ompi/mpi/cxx/intercomm_inln.h \
+ /usr/include/openmpi/ompi/mpi/cxx/group_inln.h \
+ /usr/include/openmpi/ompi/mpi/cxx/op_inln.h \
+ /usr/include/openmpi/ompi/mpi/cxx/errhandler_inln.h \
+ /usr/include/openmpi/ompi/mpi/cxx/status_inln.h \
+ /usr/include/openmpi/ompi/mpi/cxx/info_inln.h \
+ /usr/include/openmpi/ompi/mpi/cxx/win_inln.h \
+ /usr/include/openmpi/ompi/mpi/cxx/file_inln.h \
+ /usr/lib/gcc/x86_64-unknown-linux-gnu/5.1.0/include/stdint.h \
+ /usr/include/stdint.h mapreduce.h keyvalue.h
